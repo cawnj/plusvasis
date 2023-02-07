89669b18d67d284877aeadd6a4b165b3cbbf84e6
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-02-07 19:08:13 +0000

    svelte: create xterm component

--- src/svelte-app/src/routes/+page.svelte
+++ src/svelte-app/src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<script>
-    function start_container() {
-    }
+<script lang="ts">
+	import ExecController from '../lib/ExecController.svelte';
+
+	let execControllerComponent: ExecController;
+
+	function start_container() {
+		execControllerComponent.write('Starting container...');
+	}
 </script>
 
-<h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 lg:mb-6 md:text-5xl xl:text-6xl dark:text-white">
-    Continens
-</h1>
-<button class="btn btn-blue" on:click={start_container}>
-    Run
-</button>
+<h1 class="mb-4 text-4xl font-bold font-sans text-white">Continens</h1>
+<button class="mb-4 btn btn-blue" on:click={start_container}>Start</button>
+<ExecController bind:this={execControllerComponent} />
