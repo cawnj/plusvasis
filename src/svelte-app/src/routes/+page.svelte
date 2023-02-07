d07cc3550a6308ac3ecca8b9af43e477f361c87b
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-02-07 18:07:17 +0000

    svelte: init homepage and create button

--- src/svelte-app/src/routes/+page.svelte
+++ src/svelte-app/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    function start_container() {
+    }
+</script>
+
+<h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 lg:mb-6 md:text-5xl xl:text-6xl dark:text-white">
+    Continens
+</h1>
+<button class="btn btn-blue" on:click={start_container}>
+    Run
+</button>
