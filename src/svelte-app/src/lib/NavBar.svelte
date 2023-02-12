293233a8031b85d8e22c2305fb0378c46d35e060
Jason Henderson <jhenderson6810@me.com>
2023-02-12 21:58:58 +0000

    svelte-app: navbar skeleton

---
+++ src/svelte-app/src/lib/NavBar.svelte
@@ -0,0 +1,18 @@
+<div>
+	<div>
+		<nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+			<div class="flex items-center justify-between">
+				<a class="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400" href="/"
+					>Continens
+				</a>
+			</div>
+
+			<div
+				class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
+			>
+				<a class="text-gray-800 hover:text-blue-400" href="/">Home</a>
+				<a class="text-gray-800 hover:text-blue-400" href="/">About</a>
+			</div>
+		</nav>
+	</div>
+</div>
--- src/svelte-app/src/routes/+page.svelte
+++ src/svelte-app/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ExecController from '../lib/ExecController.svelte';
+	import Nav from '../lib/NavBar.svelte';
 
 	let execControllerComponent: ExecController;
 
@@ -8,6 +9,7 @@
 	}
 </script>
 
+<Nav />
 <h1 class="mb-4 text-4xl font-bold font-sans text-white">Continens</h1>
 <button class="mb-4 btn btn-blue" on:click={start_container}>Start</button>
 <ExecController bind:this={execControllerComponent} />
