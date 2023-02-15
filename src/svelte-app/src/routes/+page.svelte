3af4c5c2a25f8964c964a7a27b3fd4d0bc41d5ca
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-02-15 19:51:59 +0000

    svelte/navbar: pull button styling out into auxiliary classes

--- src/svelte-app/src/routes/+page.svelte
+++ src/svelte-app/src/routes/+page.svelte
@@ -23,11 +23,7 @@
 {#if clicked}
 	{#each jobs as job}
 		<ul>
-			<button
-				type="button"
-				class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-				on:click={start_container}>{job.ID}</button
-			>
+			<button type="button" class="btn-purple" on:click={start_container}>{job.ID}</button>
 		</ul>
 	{/each}
 {/if}
