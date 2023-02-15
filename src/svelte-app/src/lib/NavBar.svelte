212e585694672593681068803b9b6197c45f4521
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-02-15 19:33:44 +0000

    svelte/navbar: place logo and title in anchored div

--- src/svelte-app/src/lib/NavBar.svelte
+++ src/svelte-app/src/lib/NavBar.svelte
@@ -5,12 +5,14 @@
 <div>
 	<div>
 		<nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
-			<div class="flex items-center justify-between">
-				<img alt="The project logo" src={logo} class="mr-3 h-6 sm:h-9" />
-				<a class="text-xl font-bold text-white md:text-2xl hover:text-blue-400" href="/"
-					>Continens
-				</a>
-			</div>
+			<a href="/">
+				<div class="flex items-center justify-between">
+					<img alt="The project logo" src={logo} class="mr-3 h-6 sm:h-9 float-left" />
+					<span class="text-xl font-bold text-white md:text-2xl hover:text-blue-400"
+						>Continens
+					</span>
+				</div>
+			</a>
 
 			<div
 				class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
