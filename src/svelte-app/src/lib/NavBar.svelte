c9031d8bc40664d2b2924c7b8ccc20d0e2a9e6b6
Jason Henderson <jhenderson6810@me.com>
2023-02-15 12:52:11 +0000

    svelte-app: navbar logo

--- src/svelte-app/src/lib/NavBar.svelte
+++ src/svelte-app/src/lib/NavBar.svelte
@@ -1,8 +1,13 @@
+<script>
+	import logo from '$lib/assets/logo.png';
+</script>
+
 <div>
 	<div>
 		<nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
 			<div class="flex items-center justify-between">
-				<a class="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400" href="/"
+				<img alt="The project logo" src={logo} class="mr-3 h-6 sm:h-9" />
+				<a class="text-xl font-bold text-white md:text-2xl hover:text-blue-400" href="/"
 					>Continens
 				</a>
 			</div>
@@ -10,8 +15,8 @@
 			<div
 				class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
 			>
-				<a class="text-gray-800 hover:text-blue-400" href="/">Home</a>
-				<a class="text-gray-800 hover:text-blue-400" href="/">About</a>
+				<a class="text-white hover:text-blue-400" href="/">Home</a>
+				<a class="text-white hover:text-blue-400" href="/">About</a>
 			</div>
 		</nav>
 	</div>
