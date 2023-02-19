d916d708d84709fc862b61e542ee3519d8ffc82e
Jason Henderson <jhenderson6810@me.com>
2023-02-19 21:00:34 +0000

    svelte-app: firebase init

--- src/svelte-app/src/routes/+layout.svelte
+++ src/svelte-app/src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.css';
+	//import App from './fb';
+	import { onMount } from 'svelte';
+	import { getAuth, onAuthStateChanged } from 'firebase/auth';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				console.log('Welcome to Continens');
+			} else {
+				goto('/login');
+			}
+		});
+	});
 </script>
 
 <slot />
