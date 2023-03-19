7ca1254d283abdedebe1d958ee50dba792f52388
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-03-19 22:54:54 +0000

    api: dedupe some nomad controller code

--- src/go-rest-api/internal/controllers/nomad/nomad.go
+++ src/go-rest-api/internal/controllers/nomad/nomad.go
@@ -1,6 +1,7 @@
 package nomad
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -13,211 +14,65 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func GetJobs(c echo.Context) error {
+const NOMAD_URL = "https://nomad.local.cawnj.dev/v1"
 
-	resp, err := http.Get("https://nomad.local.cawnj.dev/v1/jobs")
-	if err != nil {
-		log.Println("[nomad/GetJobs]", err)
-		return err
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("[nomad/GetJobs]", err)
-		return err
-	}
-	var data interface{}
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		log.Println("[nomad/GetJobs]", err)
-		return err
-	}
-	return c.JSON(http.StatusOK, data)
-}
-
-func CreateJob(c echo.Context) error {
-	var j templates.NomadJob
-	var unmarshalErr *json.UnmarshalTypeError
-
-	decoder := json.NewDecoder(c.Request().Body)
-	decoder.DisallowUnknownFields()
-	err := decoder.Decode(&j)
-
-	if err != nil {
-		if errors.As(err, &unmarshalErr) {
-			errorResponse(c.Response().Writer, "Bad Request. Wrong Type provided for field "+unmarshalErr.Field, http.StatusBadRequest)
-		} else {
-			errorResponse(c.Response().Writer, "Bad Request "+err.Error(), http.StatusBadRequest)
-		}
-	}
-
-	data, err := templates.CreateJobJson(j)
-	if err != nil {
-		log.Println("[nomad/CreateJob]", err)
-		return err
-	}
-
-	resp, err := http.Post("https://nomad.local.cawnj.dev/v1/jobs", "application/json", data)
-	if err != nil {
-		log.Println("[nomad/CreateJob]", err)
-		return err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("[nomad/CreateJob]", err)
-		return err
-	}
-	var res interface{}
-	err = json.Unmarshal(body, &res)
-	if err != nil {
-		log.Println("[nomad/CreateJob]", err)
-		return err
-	}
-	return c.JSON(http.StatusOK, res)
-}
-
-func UpdateJob(c echo.Context) error {
-	var j templates.NomadJob
-	var unmarshalErr *json.UnmarshalTypeError
-
-	decoder := json.NewDecoder(c.Request().Body)
-	decoder.DisallowUnknownFields()
-	err := decoder.Decode(&j)
-
-	if err != nil {
-		if errors.As(err, &unmarshalErr) {
-			errorResponse(c.Response().Writer, "Bad Request. Wrong Type provided for field "+unmarshalErr.Field, http.StatusBadRequest)
-		} else {
-			errorResponse(c.Response().Writer, "Bad Request "+err.Error(), http.StatusBadRequest)
-		}
-	}
-
-	data, err := templates.CreateJobJson(j)
-	if err != nil {
-		log.Println("[nomad/CreateJob]", err)
-		return err
-	}
-
-	id := c.Param("id")
-	url := fmt.Sprintf("https://nomad.local.cawnj.dev/v1/job/%s", id)
-	resp, err := http.Post(url, "application/json", data)
-	if err != nil {
-		log.Println("[nomad/UpdateJob]", err)
-		return err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("[nomad/UpdateJob]", err)
-		return err
-	}
-	var res interface{}
-	err = json.Unmarshal(body, &res)
-	if err != nil {
-		log.Println("[nomad/UpdateJob]", err)
-		return err
-	}
-	return c.JSON(http.StatusOK, res)
-}
-
-func ReadJob(c echo.Context) error {
-	id := c.Param("id")
-	url := fmt.Sprintf("https://nomad.local.cawnj.dev/v1/job/%s", id)
+func nomadGet(endpoint string) (interface{}, error) {
+	url := NOMAD_URL + endpoint
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Println("[nomad/ReadJob]", err)
-		return err
+		return nil, err
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("[nomad/ReadJob]", err)
-		return err
+		return nil, err
 	}
 	var data interface{}
 	err = json.Unmarshal(body, &data)
-	if data != nil {
-		if err != nil {
-			log.Println("[nomad/ReadJob]", err)
-			return err
-		}
+	if err != nil {
+		return nil, err
 	}
-	if data == nil {
-		encodedJSON := []byte(`{
-			"Response" : "Job Not Found"
-		}`)
-
-		return c.JSONBlob(http.StatusBadRequest, encodedJSON)
-	}
-	return c.JSON(http.StatusOK, data)
+	return data, nil
 }
 
-func StopJob(c echo.Context) error {
-	id := c.Param("id")
-	url := fmt.Sprintf("https://nomad.local.cawnj.dev/v1/job/%s", id)
+func nomadPost(endpoint string, reqBody *bytes.Buffer) (interface{}, error) {
+	url := NOMAD_URL + endpoint
+	resp, err := http.Post(url, "application/json", reqBody)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var data interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
 
-	// Create client
-	client := &http.Client{}
-
-	// Create request
+func nomadDelete(endpoint string) (interface{}, error) {
+	url := NOMAD_URL + endpoint
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
-		log.Println("[nomad/StopJob]", err)
-		return err
+		return nil, err
 	}
-
-	// Fetch Request
+	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("[nomad/StopJob]", err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Read Response Body
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("[nomad/StopJob]", err)
-		panic(err.Error())
-	}
-	var data interface{}
-	err = json.Unmarshal(respBody, &data)
-	if err != nil {
-		log.Println("[nomad/StopJob]", err)
-		return err
-	}
-
-	return c.JSON(http.StatusOK, data)
-}
-
-func ReadJobAllocs(c echo.Context) error {
-	id := c.Param("id")
-	url := fmt.Sprintf("https://nomad.local.cawnj.dev/v1/job/%s/allocations", id)
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Println("[nomad/ReadJobAllocs]", err)
-		return err
+		return nil, err
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("[nomad/ReadJobAllocs]", err)
-		return err
+		return nil, err
 	}
 	var data interface{}
 	err = json.Unmarshal(body, &data)
-	if data != nil {
-		if err != nil {
-			log.Println("[nomad/ReadJobAllocs]", err)
-			return err
-		}
+	if err != nil {
+		return nil, err
 	}
-	if data == nil {
-		encodedJSON := []byte(`{
-			"Response" : "Job Not Found"
-		}`)
-
-		return c.JSONBlob(http.StatusBadRequest, encodedJSON)
-	}
-	return c.JSON(http.StatusOK, data)
+	return data, nil
 }
 
 func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
@@ -230,4 +85,109 @@ func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
 	if err != nil {
 		log.Println("[nomad/errorResponse]", err)
 	}
+}
+
+func GetJobs(c echo.Context) error {
+	data, err := nomadGet("/jobs")
+	if err != nil {
+		log.Println("[nomad/GetJobs]", err)
+		return err
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func CreateJob(c echo.Context) error {
+	var job templates.NomadJob
+	var unmarshalErr *json.UnmarshalTypeError
+
+	decoder := json.NewDecoder(c.Request().Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&job)
+
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			errorResponse(c.Response().Writer, "Bad Request. Wrong Type provided for field "+unmarshalErr.Field, http.StatusBadRequest)
+		} else {
+			errorResponse(c.Response().Writer, "Bad Request "+err.Error(), http.StatusBadRequest)
+		}
+	}
+
+	body, err := templates.CreateJobJson(job)
+	if err != nil {
+		log.Println("[nomad/CreateJob]", err)
+		return err
+	}
+
+	data, err := nomadPost("/jobs", body)
+	if err != nil {
+		log.Println("[nomad/CreateJob]", err)
+		return err
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func UpdateJob(c echo.Context) error {
+	var job templates.NomadJob
+	var unmarshalErr *json.UnmarshalTypeError
+
+	decoder := json.NewDecoder(c.Request().Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&job)
+
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			errorResponse(c.Response().Writer, "Bad Request. Wrong Type provided for field "+unmarshalErr.Field, http.StatusBadRequest)
+		} else {
+			errorResponse(c.Response().Writer, "Bad Request "+err.Error(), http.StatusBadRequest)
+		}
+	}
+
+	body, err := templates.CreateJobJson(job)
+	if err != nil {
+		log.Println("[nomad/CreateJob]", err)
+		return err
+	}
+
+	data, err := nomadPost(fmt.Sprintf("/job/%s", c.Param("id")), body)
+	if err != nil {
+		log.Println("[nomad/UpdateJob]", err)
+		return err
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func ReadJob(c echo.Context) error {
+	data, err := nomadGet(fmt.Sprintf("/job/%s", c.Param("id")))
+	if err != nil {
+		log.Println("[nomad/ReadJob]", err)
+		return err
+	}
+
+	if data == nil {
+		return c.JSONBlob(http.StatusBadRequest, []byte(`{ "Response" : "Job Not Found" }`))
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func StopJob(c echo.Context) error {
+	data, err := nomadDelete(fmt.Sprintf("/job/%s?purge=true", c.Param("id")))
+	if err != nil {
+		log.Println("[nomad/StopJob]", err)
+		return err
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func ReadJobAllocs(c echo.Context) error {
+
+	data, err := nomadGet(fmt.Sprintf("/job/%s/allocations", c.Param("id")))
+	if err != nil {
+		log.Println("[nomad/ReadJobAllocs]", err)
+		return err
+	}
+
+	if data == nil {
+		return c.JSONBlob(http.StatusBadRequest, []byte(`{ "Response" : "Job Not Found" }`))
+	}
+	return c.JSON(http.StatusOK, data)
 }
