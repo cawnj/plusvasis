a9cc23d1030fb91392e66c5a1b26d1effa031a3f
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-03-19 22:13:05 +0000

    svelte: load jobs onMount

--- src/svelte-app/src/routes/+page.svelte
+++ src/svelte-app/src/routes/+page.svelte
@@ -3,32 +3,25 @@
 	import Nav from '$lib/NavBar.svelte';
 	import NomadController from '$lib/NomadController.svelte';
 	import xtermIcon from '$lib/assets/xTerm.png';
-
-	let getContainerClicked: boolean;
-	let getContainerCreatedClicked: boolean;
-	let containerRunning: boolean;
-
-	export let data;
-	const { jobs } = data;
+	import { onMount } from 'svelte';
+	import { hostname } from '../stores/environmentStore';
 
 	let nomadControllerComponent: NomadController;
+
+	let jobs: any[] = [];
+	onMount(async () => {
+		const res = await fetch(`${hostname}/jobs`);
+		jobs = await res.json();
+	});
 </script>
 
 <Nav />
-<button class="mb-4 btn btn-blue" on:click={nomadControllerComponent.getContainers}
-	>Get Containers</button
->
-<button class="mb-4 btn btn-blue" on:click={nomadControllerComponent.getContainerCreated}
+<button class="mb-4 btn btn-blue" on:click={() => goto('/create')}
 	>Create Container</button
 >
-{#if getContainerClicked}
-	{#each nomadControllerComponent.parseData(jobs) as job}
+{#each jobs as job}
+	{#if job.Meta && job.Meta.user === localStorage.getItem('uid')}
 		<ul>
-			<!-- <button
-				type="button"
-				class="btn-purple"
-				on:click={() => nomadControllerComponent.fetchJobIdAllocations(job.ID)}>{job.ID}</button
-			> -->
 			<div class="mt-3">
 				<div class="div-container">
 					<a href="/container/{job.ID}">
@@ -42,22 +35,5 @@
 				</div>
 			</div>
 		</ul>
-	{/each}
-{/if}
-{#if getContainerCreatedClicked}
-	{goto('/create')}
-{/if}
-<!-- disabled for the moment, breaks things -->
-<!-- {#if containerRunning}
-	<button
-		type="button"
-		class="btn-red"
-		on:click={() => nomadControllerComponent.fetchJobIdDelete(job)}>Stop Container {job}</button
-	>
-{/if} -->
-<NomadController
-	bind:this={nomadControllerComponent}
-	bind:getContainerClicked
-	bind:getContainerCreatedClicked
-	bind:containerRunning
-/>
+	{/if}
+{/each}
