c2b34330752df7210695c64722f0f40b7895a958
Jason Henderson <jhenderson6810@me.com>
2023-03-28 19:36:36 +0100

    api: Test GetJobs

--- src/go-rest-api/internal/test/api_test.go
+++ src/go-rest-api/internal/test/api_test.go
@@ -1,12 +1,17 @@
 package test
 
 import (
+	"continens/internal/controllers/nomad"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	mockJob = `'test': 'job'`
 )
 
 func health(c echo.Context) error {
@@ -22,6 +27,20 @@ func TestHealth(t *testing.T) {
 
 	// Assertions
 	if assert.NoError(t, health(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestGetJobs(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/jobs")
+	c.Set("uid", "test")
+
+	// Assertions
+	if assert.NoError(t, nomad.GetJobs(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 }
