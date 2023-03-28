7f50029d991c1d6934c5f71e408030130c7aa537
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-03-28 05:30:58 +0100

    svelte: prettier-ify

--- src/svelte-app/src/lib/Types.ts
+++ src/svelte-app/src/lib/Types.ts
@@ -17,55 +17,55 @@ type JobField = {
 	info: string;
 };
 
-export const JobFields: {key: string, value: JobField}[] = [
+export const JobFields: { key: string; value: JobField }[] = [
 	{
-	  	key: 'containerName',
-	  	value: {
+		key: 'containerName',
+		value: {
 			title: 'Container Name',
 			placeholder: 'alpine',
-			info: 'This is used to identify the container.',
-		},
+			info: 'This is used to identify the container.'
+		}
 	},
 	{
 		key: 'dockerImage',
 		value: {
 			title: 'Docker Image',
 			placeholder: 'alpine:latest',
-			info: 'Images will be pulled from a registry, e.g. Docker Hub.',
-		},
+			info: 'Images will be pulled from a registry, e.g. Docker Hub.'
+		}
 	},
 	{
 		key: 'shell',
 		value: {
 			title: 'Shell',
 			placeholder: '/bin/sh',
-			info: 'The shell to use when executing commands within the container.',
-		},
+			info: 'The shell to use when executing commands within the container.'
+		}
 	},
 	{
 		key: 'volumes',
 		value: {
 			title: 'Volumes',
 			placeholder: 'docker_volume:/mnt/volume',
-			info: 'For persistant storage, volumes are required. You can add multiple volumes by separating them with a comma.',
-		},
+			info: 'For persistant storage, volumes are required. You can add multiple volumes by separating them with a comma.'
+		}
 	},
 	{
 		key: 'env',
 		value: {
 			title: 'Environment Variables',
 			placeholder: 'ENV_VAR=VALUE',
-			info: 'These will be passed to the container. You can add multiple environment variables by separating them with a comma.',
-		},
+			info: 'These will be passed to the container. You can add multiple environment variables by separating them with a comma.'
+		}
 	},
 	{
 		key: 'port',
 		value: {
 			title: 'Port',
 			placeholder: '8080',
-			info: 'The port to expose to the host.',
-		},
-	},
+			info: 'The port to expose to the host.'
+		}
+	}
 ];
 
 export type Tab = {
