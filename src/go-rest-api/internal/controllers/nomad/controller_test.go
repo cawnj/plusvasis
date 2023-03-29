8a34d1dd6b4ab9a437372edd046b0d66a235bd09
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-03-29 03:13:22 +0100

    api: add TestCreateJob

--- src/go-rest-api/internal/controllers/nomad/controller_test.go
+++ src/go-rest-api/internal/controllers/nomad/controller_test.go
@@ -3,9 +3,12 @@ package nomad
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"plusvasis/internal/templates"
 
 	nomad "github.com/hashicorp/nomad/nomad/structs"
 	"github.com/labstack/echo/v4"
@@ -71,6 +74,40 @@ func TestGetJobs(t *testing.T) {
 	expectedJson, _ := json.Marshal(expected)
 	code := http.StatusOK
 	if assert.NoError(t, nomadController.GetJobs(c)) {
+		assert.Equal(t, code, rec.Code)
+		assert.JSONEq(t, string(expectedJson), rec.Body.String())
+	}
+}
+
+func TestCreateJob(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/jobs", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Mock NomadJob POST body
+	nomadJob := templates.NomadJob{
+		Name: "test",
+	}
+	nomadJobJson, _ := json.Marshal(nomadJob)
+	c.Request().Body = io.NopCloser(bytes.NewBuffer(nomadJobJson))
+
+	// Mock NomadClient
+	nomadRegister := nomad.JobRegisterResponse{
+		EvalID:          "test",
+		EvalCreateIndex: 1,
+		JobModifyIndex:  1,
+	}
+	nomadRegisterJson, _ := json.Marshal(nomadRegister)
+	nomadClient := new(MockNomadClient)
+	nomadClient.On("Post", "/jobs", mock.Anything).Return(nomadRegisterJson, nil)
+	nomadController := NomadController{nomadClient}
+
+	// Assertions
+	expectedJson := nomadRegisterJson
+	code := http.StatusOK
+	if assert.NoError(t, nomadController.CreateJob(c)) {
 		assert.Equal(t, code, rec.Code)
 		assert.JSONEq(t, string(expectedJson), rec.Body.String())
 	}
