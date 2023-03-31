66d435cf4dfd66efb340136ed22a2608dac7d99a
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-03-31 08:20:08 +0100

    svelte: add /about and /logout

---
+++ src/svelte-app/src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Nav from '$lib/NavBar.svelte';
+</script>
+
+<Nav />
+<div class="px-8 md:px-16 text-white">
+	<h1 class="text-3xl md:text-5xl font-bold">About</h1>
+	<p class="text-xl md:text-2xl">This is the about page.</p>
+</div>
