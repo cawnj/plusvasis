cf580fa7c691c63ce814820aaf7ae58395ce0dc0
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-18 04:38:45 +0100

    svelte: recenter login and signuo forms

--- src/svelte-app/src/routes/signup/+page.svelte
+++ src/svelte-app/src/routes/signup/+page.svelte
@@ -2,6 +2,6 @@
 	import Login from '$lib/LoginForm.svelte';
 </script>
 
-<div class="px-4 py-4 md:px-16 md:py-16">
+<div class="px-4 py-4 md:px-16 md:py-16 grid place-items-center">
 	<Login title="Sign Up" />
 </div>
