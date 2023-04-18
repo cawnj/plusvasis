8ee8c4f9482630462de3e3ff6b0a1020ae49c150
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-18 04:30:28 +0100

    svelte: remove dropdown in favour of responsive buttons

--- src/svelte-app/src/lib/ContainerOptions.svelte
+++ src/svelte-app/src/lib/ContainerOptions.svelte
@@ -8,62 +8,30 @@
 	import { Dropdown, DropdownItem } from 'flowbite-svelte';
 </script>
 
-<div class="md:hidden">
-	<Button size="xs"><Chevron>Options</Chevron></Button>
-	<Dropdown class="w-24 py-1">
-		<DropdownItem on:click={() => fetchJobStart().then(() => window.location.reload())}>
-			<div class="flex items-center text-xs">
-				<Fa icon={faPlay} color="green" class="mr-2" />
-				Start
-			</div>
-		</DropdownItem>
-		<DropdownItem on:click={() => fetchJobRestart().then(() => window.location.reload())}>
-			<div class="flex items-center text-xs">
-				<Fa icon={faRefresh} color="orange" class="mr-2" />
-				Restart
-			</div>
-		</DropdownItem>
-		<DropdownItem on:click={() => fetchJobStop().then(() => window.location.reload())}>
-			<div class="flex items-center text-xs">
-				<Fa icon={faStop} color="red" class="mr-2" />
-				Stop
-			</div>
-		</DropdownItem>
-		<DropdownItem on:click={() => fetchJobDelete().then(() => goto('/'))}>
-			<div class="flex items-center text-xs">
-				<Fa icon={faTrash} class="mr-2" />
-				Delete
-			</div>
-		</DropdownItem>
-	</Dropdown>
-</div>
-
-<div class="hidden md:block">
-	<ButtonGroup>
-		<Button
-			disabled={!$currJobStopped}
-			on:click={() => fetchJobStart().then(() => window.location.reload())}
-		>
-			<Fa icon={faPlay} color="green" class="mr-2" />
-			Start
-		</Button>
-		<Button
-			disabled={$currJobStopped}
-			on:click={() => fetchJobRestart().then(() => window.location.reload())}
-		>
-			<Fa icon={faRefresh} color="orange" class="mr-2" />
-			Restart
-		</Button>
-		<Button
-			disabled={$currJobStopped}
-			on:click={() => fetchJobStop().then(() => window.location.reload())}
-		>
-			<Fa icon={faStop} color="red" class="mr-2" />
-			Stop
-		</Button>
-		<Button on:click={() => fetchJobDelete().then(() => goto('/'))}>
-			<Fa icon={faTrash} class="mr-2" />
-			Delete
-		</Button>
-	</ButtonGroup>
-</div>
+<ButtonGroup>
+	<Button
+		disabled={!$currJobStopped}
+		on:click={() => fetchJobStart().then(() => window.location.reload())}
+	>
+		<Fa icon={faPlay} color="green" class="md:mr-2" />
+		<span class="hidden md:block">Start</span>
+	</Button>
+	<Button
+		disabled={$currJobStopped}
+		on:click={() => fetchJobRestart().then(() => window.location.reload())}
+	>
+		<Fa icon={faRefresh} color="orange" class="md:mr-2" />
+		<span class="hidden md:block">Restart</span>
+	</Button>
+	<Button
+		disabled={$currJobStopped}
+		on:click={() => fetchJobStop().then(() => window.location.reload())}
+	>
+		<Fa icon={faStop} color="red" class="md:mr-2" />
+		<span class="hidden md:block">Stop</span>
+	</Button>
+	<Button on:click={() => fetchJobDelete().then(() => goto('/'))}>
+		<Fa icon={faTrash} class="md:mr-2" />
+		<span class="hidden md:block">Delete</span>
+	</Button>
+</ButtonGroup>
