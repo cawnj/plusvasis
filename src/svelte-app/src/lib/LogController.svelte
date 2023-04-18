c37c7047addb69b16f13dc734bb9282cb2bd0da1
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-18 13:15:36 +0100

    svelte: organise LogController and create StreamLogs.ts

--- src/svelte-app/src/lib/LogController.svelte
+++ src/svelte-app/src/lib/LogController.svelte
@@ -1,68 +1,17 @@
 <script lang="ts">
-	import { currJobId, currJob } from '../stores/nomadStore';
-	import { onMount, afterUpdate } from 'svelte';
-	import { b64decode } from './Base64Util';
-	import type { ReadableStreamDefaultReader } from 'web-streams-polyfill/ponyfill';
+	import { onMount } from 'svelte';
 	import { Button, Dropdown, Chevron, Radio } from 'flowbite-svelte';
-	import { hostname } from '../stores/environmentStore';
-	import type { Job } from './Types';
+	import { decode, getReader } from './StreamLogs';
 
-	let jobId: string;
-	let job: Job;
 	let type = 'stdout';
-
-	currJobId.subscribe((value) => {
-		jobId = value;
-	});
-	currJob.subscribe((value) => {
-		job = value;
-	});
-
 	let logs = '';
+
 	let preEl: HTMLPreElement;
-	let reader: ReadableStreamDefaultReader<Uint8Array>;
+	let reader: ReadableStreamDefaultReader;
 
-	// https://github.com/hashicorp/nomad/blob/main/ui/app/utils/stream-frames.js
-	function decode(chunk: string): { offset: number; message: string } | null {
-		const lines = chunk.replace(/\}\{/g, '}\n{').split('\n').filter(Boolean);
-		const frames = lines.map((line) => JSON.parse(line)).filter((frame) => frame.Data);
-
-		if (frames.length) {
-			frames.forEach((frame) => (frame.Data = b64decode(frame.Data)));
-			return {
-				offset: frames[frames.length - 1].Offset,
-				message: frames.map((frame) => frame.Data).join('')
-			};
-		}
-
-		return null;
-	}
-
-	const fetchLogs = async () => {
-		const urlBuilder = new URL(`${hostname}/job/${jobId}/logs`);
-		urlBuilder.searchParams.append('task', job.containerName);
-		urlBuilder.searchParams.append('type', type);
-		urlBuilder.searchParams.append('follow', 'true');
-		urlBuilder.searchParams.append('offset', '50000');
-		urlBuilder.searchParams.append('origin', 'end');
-		const url = urlBuilder.toString();
-
-		const logFetch = async (url: string) => {
-			const response = await fetch(url);
-			if (response.ok) {
-				return response;
-			} else {
-				throw new Error(response.statusText);
-			}
-		};
-
-		const readerResponse = await logFetch(url);
-		if (!readerResponse.body) {
-			throw new Error('No response body');
-		}
-
-		// https://github.com/hashicorp/nomad/blob/main/ui/app/utils/classes/stream-logger.js
-		reader = readerResponse.body.getReader();
+	// https://github.com/hashicorp/nomad/blob/main/ui/app/utils/classes/stream-logger.js
+	async function readLogs() {
+		logs = '';
 		let streamClosed = false;
 		let buffer = '';
 		const decoder = new TextDecoder();
@@ -83,35 +32,32 @@
 				}
 			}
 		}
-	};
+	}
 
-	const handleChange = async () => {
-		logs = '';
-		if (reader) {
-			reader.cancel();
-		}
-		await fetchLogs();
-	};
+	async function changeType() {
+		if (reader) reader.cancel();
+		reader = await getReader(type);
+		readLogs();
+	}
 
-	onMount(() => {
-		fetchLogs();
+	onMount(async () => {
+		reader = await getReader(type);
+		readLogs();
 	});
 
-	afterUpdate(() => {
-		if (preEl) {
-			preEl.scrollTop = preEl.scrollHeight;
-		}
-	});
+	$: {
+		if (preEl) preEl.scrollTop = preEl.scrollHeight;
+	}
 </script>
 
 <div class="pb-4">
 	<Button><Chevron>{type}</Chevron></Button>
 	<Dropdown class="w-32 p-3 space-y-3 text-sm">
 		<li>
-			<Radio name="type" bind:group={type} value={'stdout'} on:change={handleChange}>stdout</Radio>
+			<Radio name="type" bind:group={type} value={'stdout'} on:change={changeType}>stdout</Radio>
 		</li>
 		<li>
-			<Radio name="type" bind:group={type} value={'stderr'} on:change={handleChange}>stderr</Radio>
+			<Radio name="type" bind:group={type} value={'stderr'} on:change={changeType}>stderr</Radio>
 		</li>
 	</Dropdown>
 </div>
