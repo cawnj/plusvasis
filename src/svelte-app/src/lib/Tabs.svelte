432b85b16de20ebfffda26cf4a69e7905207f17d
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-18 05:57:32 +0100

    svelte: responsive tab text size

--- src/svelte-app/src/lib/Tabs.svelte
+++ src/svelte-app/src/lib/Tabs.svelte
@@ -10,6 +10,13 @@
 	{#if !tabs || tabs.length === 0}
 		<TabItem open>
 			<div slot="title">No tabs</div>
+	{#each tabs as tab, index}
+		<TabItem open={index === 0}>
+			<div slot="title" class="flex items-center gap-2 text-xs md:text-sm">
+				<Fa icon={tab.icon} color="white" />
+				{tab.name}
+			</div>
+			<svelte:component this={tab.component} />
 		</TabItem>
 	{:else}
 		{#each tabs as tab, index}
