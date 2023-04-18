ac3c698f29b9b4c4b1fe1ace2faac650b2110190
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-18 13:17:43 +0100

    svelte: remove unnecessary nomadStore writables

--- src/svelte-app/src/stores/nomadStore.ts
+++ src/svelte-app/src/stores/nomadStore.ts
@@ -3,7 +3,4 @@ import { writable } from 'svelte/store';
 
 export const currJobId = writable('');
 export const currJob = writable({} as Job);
-export const shell = writable('');
-export const alloc = writable('');
-export const task = writable('');
 export const currJobStopped = writable(false);
