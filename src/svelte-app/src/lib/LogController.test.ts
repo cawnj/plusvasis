5dfc03a51ee7b3f3324894a9c5d1fe602856859b
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-19 03:47:03 +0100

    svelte: mock StreamLogs in test
    Great example to use in future for mocking an exported function from an
    imported file using vitest mocks.

--- src/svelte-app/src/lib/LogController.test.ts
+++ src/svelte-app/src/lib/LogController.test.ts
@@ -1,8 +1,16 @@
-import { describe, it, expect } from 'vitest';
+import { vi, describe, it, expect } from 'vitest';
 import { render } from '@testing-library/svelte';
 import { currJobId, currJob } from '../stores/nomadStore';
 import LogController from './LogController.svelte';
 import type { Job } from '$lib/Types';
+
+vi.mock('./StreamLogs', () => {
+	const actual = vi.importActual('./StreamLogs');
+	return {
+		...actual,
+		getStream: vi.fn(() => Promise.resolve(new ReadableStream()))
+	};
+});
 
 describe('LogController', () => {
 	it('should render component', async () => {
