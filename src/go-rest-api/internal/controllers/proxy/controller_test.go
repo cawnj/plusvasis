174e9f47636e670eef81c690d506fed2e84c2c15
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-20 01:49:08 +0100

    api: init TestAllocExec

---
+++ src/go-rest-api/internal/controllers/proxy/controller_test.go
@@ -0,0 +1,72 @@
+package proxy
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	nomad "github.com/hashicorp/nomad/nomad/structs"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockNomadClient struct {
+	mock.Mock
+}
+
+func (m *MockNomadClient) Get(endpoint string) ([]byte, error) {
+	args := m.Called(endpoint)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (m *MockNomadClient) Post(endpoint string, reqBody *bytes.Buffer) ([]byte, error) {
+	args := m.Called(endpoint, reqBody)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (m *MockNomadClient) Delete(endpoint string) ([]byte, error) {
+	args := m.Called(endpoint)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func setup(method string, url string) (
+	*httptest.ResponseRecorder, echo.Context, *MockNomadClient, NomadProxyController,
+) {
+	e := echo.New()
+	req := httptest.NewRequest(method, url, nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	nomadClient := new(MockNomadClient)
+	nomadController := NomadProxyController{nomadClient}
+
+	return rec, c, nomadClient, nomadController
+}
+
+func TestAllocExec(t *testing.T) {
+	// Setup
+	jobName := "test"
+	rec, c, nomadClient, nomadProxyController := setup(http.MethodGet, "/job/"+jobName)
+	c.SetParamNames("id")
+	c.SetParamValues(jobName)
+	c.QueryParams().Set("command", "[\"/bin/bash\"]")
+
+	// Mocks
+	nomadJobAlloc := []nomad.AllocListStub{
+		{
+			ID:           "test",
+			ClientStatus: "running",
+		},
+	}
+	allocsJson, _ := json.Marshal(nomadJobAlloc)
+	nomadClient.On("Get", "/job/"+jobName+"/allocations").Return(allocsJson, nil)
+
+	// Assertions
+	expectedCode := http.StatusOK
+	if assert.NoError(t, nomadProxyController.AllocExec(c)) {
+		assert.Equal(t, expectedCode, rec.Code)
+	}
+}
