4ce0121ccfc7baa21037d1a41387ffcb15e56828
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-23 06:56:25 +0100

    api: organise main.go

--- src/go-rest-api/cmd/server/main.go
+++ src/go-rest-api/cmd/server/main.go
@@ -10,8 +10,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	e := echo.New()
+func setupMiddlewares(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{
 			"http://localhost:5173",
@@ -19,17 +18,25 @@ func main() {
 		},
 		AllowHeaders: []string{"*"},
 	}))
-
 	e.Use(middleware.Recover())
 	e.Use(middleware.Secure())
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
 
 	e.Use(customMw.Logger())
 	e.Use(customMw.Firebase())
+}
 
+func setupRoutes(e *echo.Echo) {
 	routes.HealthRoutes(e)
 	routes.NomadJobs(e)
 	routes.NomadProxy(e)
+}
+
+func main() {
+	e := echo.New()
+
+	setupMiddlewares(e)
+	setupRoutes(e)
 
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
