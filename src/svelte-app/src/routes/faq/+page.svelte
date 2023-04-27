030963f549a60b30c6b93a380ccaeb711dbea321
Jason Henderson <jhenderson6810@me.com>
2023-04-27 22:38:18 +0100

    svelte: add faq page

---
+++ src/svelte-app/src/routes/faq/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Nav from '$lib/NavBar.svelte';
+</script>
+
+<Nav />
+<div class="px-4 md:px-16 mb-4 text-white">
+	<h1 class="text-xl md:text-2xl font-bold">FAQ</h1>
+	<p class="font-medium">This is the FAQ page.</p>
+</div>
