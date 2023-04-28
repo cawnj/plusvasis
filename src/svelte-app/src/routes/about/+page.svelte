fca75b5c9fa52dabb73d5b919ebe5f72df4e7baf
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-28 01:01:17 +0100

    svelte: use each loop for about page cards

--- src/svelte-app/src/routes/about/+page.svelte
+++ src/svelte-app/src/routes/about/+page.svelte
@@ -8,6 +8,33 @@
 		faBarsProgress
 	} from '@fortawesome/free-solid-svg-icons';
 	import { Card, Heading, Span } from 'flowbite-svelte';
+
+	const cards = [
+		{
+			icon: faEarthAmericas,
+			title: 'Intuitive Platform',
+			description:
+				'PlusVasis is an intuitive online platform that simplifies container management and orchestration for developers.'
+		},
+		{
+			icon: faNetworkWired,
+			title: 'Seamless Networking',
+			description:
+				'PlusVasis offers seamless networking, security, and scalability, allowing users to spin up containers for their websites or databases effortlessly.'
+		},
+		{
+			icon: faGears,
+			title: 'Modern Infrastructure',
+			description:
+				'PlusVasis is powered by modern frameworks and infrastructure solutions, making it easy for developers to manage their containers without worrying about complex backend processes.'
+		},
+		{
+			icon: faBarsProgress,
+			title: 'Streamlined Management',
+			description:
+				'PlusVasis streamlines container management and enables developers to focus on building their applications.'
+		}
+	];
 </script>
 
 <Nav />
@@ -15,70 +42,22 @@
 >
 <div class="px-4 md:px-16 space-y-8 text-white flex justify-center">
 	<div class="grid grid-cols-2 gap-8">
-		<Card horizontal>
-			<div class="flex flex-col">
-				<div class="flex items-center justify-center mb-4">
-					<Fa icon={faEarthAmericas} class="text-4xl text-sky-400 mr-4" />
-					<Heading tag="h2" class="text-2xl font-semibold">
-						<Span gradient>Intuitive Platform</Span>
-					</Heading>
+		{#each cards as card}
+			<Card horizontal>
+				<div class="flex flex-col">
+					<div class="flex items-center justify-center mb-4">
+						<Fa icon={card.icon} class="text-4xl text-sky-400 mr-4" />
+						<Heading tag="h2" class="text-2xl font-semibold">
+							<Span gradient>{card.title}</Span>
+						</Heading>
+					</div>
+					<div>
+						<p class="text-xl text-white">
+							{card.description}
+						</p>
+					</div>
 				</div>
-				<div>
-					<p class="text-xl text-white">
-						PlusVasis is an intuitive online platform that simplifies container management and
-						orchestration for developers.
-					</p>
-				</div>
-			</div>
-		</Card>
-		<Card horizontal>
-			<div class="flex flex-col">
-				<div class="flex items-center mb-4">
-					<Fa icon={faNetworkWired} class="text-4xl text-sky-400 mr-4" />
-					<Heading tag="h2" class="text-2xl font-semibold">
-						<Span gradient>Seamless Networking</Span>
-					</Heading>
-				</div>
-				<div>
-					<p class="text-xl text-white">
-						PlusVasis offers seamless networking, security, and scalability, allowing users to spin
-						up containers for their websites or databases effortlessly.
-					</p>
-				</div>
-			</div>
-		</Card>
-		<Card horizontal>
-			<div class="flex flex-col">
-				<div class="flex items-center mb-4">
-					<Fa icon={faGears} class="text-4xl text-sky-400 mr-4" />
-					<Heading tag="h2" class="text-2xl font-semibold">
-						<Span gradient>Modern Infrastructure</Span>
-					</Heading>
-				</div>
-				<div>
-					<p class="text-xl text-white">
-						PlusVasis is powered by modern frameworks and infrastructure solutions, making it easy
-						for developers to manage their containers without worrying about complex backend
-						processes.
-					</p>
-				</div>
-			</div>
-		</Card>
-		<Card horizontal>
-			<div class="flex flex-col">
-				<div class="flex items-center mb-4">
-					<Fa icon={faBarsProgress} class="text-4xl text-sky-400 mr-4" />
-					<Heading tag="h2" class="text-2xl font-semibold">
-						<Span gradient>Streamlined Management</Span>
-					</Heading>
-				</div>
-				<div>
-					<p class="text-xl text-white">
-						PlusVasis streamlines container management and enables developers to focus on building
-						their applications.
-					</p>
-				</div>
-			</div>
-		</Card>
+			</Card>
+		{/each}
 	</div>
 </div>
