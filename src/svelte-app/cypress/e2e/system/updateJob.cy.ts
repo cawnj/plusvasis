7403e3f88bdf861502485c9152d4a21837964777
Jason Henderson <jhenderson6810@me.com>
2023-05-02 13:24:08 +0100

    cypress: add update job test

---
+++ src/svelte-app/cypress/e2e/system/updateJob.cy.ts
@@ -0,0 +1,51 @@
+/* eslint-disable cypress/unsafe-to-chain-command */
+/* eslint-disable cypress/no-unnecessary-waiting */
+
+Cypress.on('window:before:load', (win) => {
+	cy.stub(win.console, 'log').as('consoleLog');
+});
+
+describe('updateJob spec', () => {
+	it('User updates a current job', () => {
+		cy.intercept(
+			'POST',
+			'https://api.plusvasis.xyz/job/lK5VKiQIR6UpBXkZ35Ccft5Uyqg1-cypress-test'
+		).as('jobUpdated');
+		cy.intercept('GET', 'https://api.plusvasis.xyz/jobs').as('jobs');
+		cy.visit('https://app.plusvasis.xyz/login');
+
+		cy.wait(2000);
+
+		cy.get('input[name="email"]').type('example@email.com');
+		cy.get('input[name="password"]').type('th1s1sJustATest');
+		cy.get('button[type="submit"]').click();
+
+		cy.wait(1500);
+
+		cy.wait('@jobs');
+		cy.get('div[data-testid="job-list"]').should('exist');
+
+		cy.get('a[href*="/container/lK5VKiQIR6UpBXkZ35Ccft5Uyqg1-cypress-test"]').click();
+
+		cy.wait(1500);
+
+		cy.contains('Settings').click();
+
+		cy.get('input[name="dockerImage"]', { timeout: 10000 })
+			.should('be.visible')
+			.clear()
+			.type('nginx', { delay: 100 })
+			.should('have.value', 'nginx');
+
+		cy.get('input[name="port"]', { timeout: 10000 })
+			.should('be.visible')
+			.clear()
+			.type('80', { delay: 100 })
+			.should('have.value', '80');
+
+		cy.get('button[type="submit"]').click();
+
+		cy.wait('@jobUpdated');
+		cy.get('@consoleLog').should('be.calledWith', 'Container Updated');
+	});
+});
