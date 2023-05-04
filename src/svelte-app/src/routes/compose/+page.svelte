d29b0a822c797f0ebb25bcf95db521fe394d1ee3
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-05-04 19:21:53 +0100

    svelte: submit docker compose jobs

--- src/svelte-app/src/routes/compose/+page.svelte
+++ src/svelte-app/src/routes/compose/+page.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
-	import { Button, Heading } from 'flowbite-svelte';
+	import { Button, Heading, Spinner } from 'flowbite-svelte';
 	import * as yaml from 'js-yaml';
 
+	import { goto } from '$app/navigation';
 	import Editor from '$lib/components/Editor.svelte';
 	import NavBar from '$lib/components/NavBar.svelte';
-	import type { DockerCompose, Job } from '$lib/types/Types';
+	import type { DockerCompose } from '$lib/types/Types';
 	import { MakeJobsFromCompose } from '$lib/utils/MakeJob';
-
-	import { currJob } from '../../stores/nomadStore';
-
-	// default job configuration
-	currJob.set({
-		shell: '/bin/sh',
-		cpu: 100,
-		memory: 300
-	} as Job);
+	import { fetchJobCreate } from '$lib/utils/NomadClient';
 
 	let dockerComposeStr: string;
+	let loading = false;
 
 	function handleSubmit() {
 		const dockerComposeYaml = yaml.load(dockerComposeStr);
 		const dockerComposeData: DockerCompose = dockerComposeYaml as DockerCompose;
 		const jobs = MakeJobsFromCompose(dockerComposeData);
-		console.log(jobs);
+
+		for (const job of jobs) {
+			fetchJobCreate(job);
+		}
+		loading = true;
+		setTimeout(() => {
+			goto('/');
+		}, 1000);
 	}
 </script>
 
@@ -31,6 +32,12 @@
 	<Heading tag="h3" class="mb-4 font-semibold text-white">docker-compose</Heading>
 	<div class="relative">
 		<Editor bind:value={dockerComposeStr} />
-		<Button class="absolute bottom-4 right-4" color="blue" on:click={handleSubmit}>Submit</Button>
+		<Button class="absolute bottom-4 right-4" color="blue" on:click={handleSubmit}>
+			{#if loading}
+				<Spinner class="mr-3" size="4" color="white" />Loading ...
+			{:else}
+				Submit
+			{/if}
+		</Button>
 	</div>
 </div>
